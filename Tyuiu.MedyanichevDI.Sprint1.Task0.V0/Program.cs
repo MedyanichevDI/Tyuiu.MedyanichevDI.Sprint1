41cb940ca37597a059067d6a9f0bce2aa30734ae
Денис <121244780+Domovoy23@users.noreply.github.com>
2024-09-16 21:30:09 +0500

    Add project files.

---
+++ Tyuiu.MedyanichevDI.Sprint1.Task0.V0/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.MedyanichevDI.Sprint1.Task0.V0.Lib;
+
+namespace Tyuiu.MedyanichevDI.Sprint1.Task0.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Медяничев Д.И. | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #0                                                              *");
+            Console.WriteLine("* Выполнил: Медяничев Денис Игоревич | АСОиУб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*Написать программу, которая вычисляет выражение 10/(2+" + "3)             *");
+            Console.WriteLine("* одинаковых массивов по длине.                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 2*3*3+7                                                                 *");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate());
+            Console.ReadKey();
+        }
+    }
+}
